968cfc8e5364d194753c2930fb7bbf5229f83f1b
João Vitor Bonadiman <j.bonadiman@live.com>
2021-03-23 21:36:41 -0300

    Adiciona estilos globais e tema claro;

---
+++ src/styles/lightTheme.ts
@@ -0,0 +1,17 @@
+const lightTheme = {
+  colors: {
+    white: '#ffffff',
+    background: '#f2f3f5',
+    grayLine: '#dcdde8',
+    text: '#666',
+    textHighlight: '#b3b9ff',
+    title: '#2e384d',
+    red: '#e83f5b',
+    green: '#4cd62b',
+    blue: '#5965e0',
+    darkBlue: '#4953b8',
+    twitterBlue: '#2aa9e0'
+  }
+}
+
+export default lightTheme
